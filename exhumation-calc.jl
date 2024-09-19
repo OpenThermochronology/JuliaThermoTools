ba4e4c0ee9b448afbcc71950de474c9fe0afe9f9
kalin mcdannell <35042250+kmcdannell@users.noreply.github.com>
2024-09-19 15:03:19 -0400

    Update exhumation-calc.jl

--- exhumation-calc.jl
+++ exhumation-calc.jl
@@ -1,10 +1,15 @@
 ## ---
+
+# A simple calculator to estimate an average (μ ± σ) exhumation depth from thermal history models
+# specifying uniform distributions (or any other distribution) for variables like surface temperature,
+# geothermal gradient, and maximum heating/burial temperature extracted from a thermal history.
+
 cd(@__DIR__)
 
 using Statistics, Distributions
 
 # Define the number of iterations
-n_iter = 1000
+n_iter = 5000
 
 # Define the range for the variables
 Ts = Uniform(0.0, 15.0)    # Surface Temperature (°C)
@@ -14,7 +19,7 @@ Tb = Uniform(110.0, 200.0) # Burial Temperature (°C)
 # Initialize an array to store erosion depth results
 erosion_depths = zeros(n_iter)
 
-# Loop to calculate erosion depth 2000 times
+# Loop to calculate erosion depth 5000 times
 for i in 1:n_iter
     # Sample random values from uniform distributions
     Tsᵢ = rand(Ts)
@@ -29,15 +34,19 @@ for i in 1:n_iter
 end
 
 # Calculate the mean and standard deviation of the results
-mean_erosion_depth = round(mean(erosion_depths), digits=2)
-std_erosion_depth = round(std(erosion_depths), digits=2)
+
 #mean_erosion_depth = mean(erosion_depths)
 #std_erosion_depth = std(erosion_depths)
+
+mean_erosion_depth = round(mean(erosion_depths), digits=2)
+std_erosion_depth = round(std(erosion_depths), digits=2)
+
 min_e = round(mean_erosion_depth - std_erosion_depth, digits=2)
 max_e = round(mean_erosion_depth + std_erosion_depth, digits=2)
 
 # Print the results
-println("Average exhumation depth: $mean_erosion_depth km")
+println("Mean exhumation depth: $mean_erosion_depth km")
 println("Standard deviation: $std_erosion_depth km")
 println("Exh minimum/maximum: $min_e km to $max_e km")
+
 ## ---
